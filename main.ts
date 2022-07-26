f98847b0a9bf51957ae399a923a935ad42e6489f
jerry22lin <110014366+jerry22lin@users.noreply.github.com>
2022-07-26 14:48:13 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+let 點數 = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(點數)
+})
+input.onGesture(Gesture.Shake, function () {
+    點數 = randint(1, 6)
+})
 basic.forever(function () {
 	
 })
