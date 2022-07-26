7c402e327534249b34b165f062e7a30431d95952
jerry22lin <110014366+jerry22lin@users.noreply.github.com>
2022-07-26 15:09:05 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -23,7 +23,7 @@ basic.forever(function () {
         basic.showLeds(`
             . . . . .
             . . . . .
-            # . # . #
+            . # # # .
             . . . . .
             . . . . .
             `)
